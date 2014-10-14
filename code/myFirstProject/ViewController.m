30a4d2142dfe99f4eb1694ea70d1de011249229c
khushboowal <khushboo@westagilelabs.com>
2014-10-14 12:02:56 +0530

    pull 2

--- code/myFirstProject/ViewController.m
+++ code/myFirstProject/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"hello");
+    NSLog(@"hello again");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
